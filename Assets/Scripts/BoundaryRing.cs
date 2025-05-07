0e8b195aff14e3cea003bfccf76b1872abffb3af
sinik111 <kangmark95@naver.com>
2025-05-07 20:05:13 +0900

    feature ThrowerMovement, BoundaryRing

---
+++ Assets/Scripts/BoundaryRing.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryRing : MonoBehaviour
+{
+    [SerializeField] private int segments;
+    [SerializeField] private float radius;
+    [SerializeField] private float width;
+
+    private LineRenderer lineRenderer;
+    private List<Vector3> vertices;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.startColor = Color.white;
+        lineRenderer.endColor = Color.white;
+        lineRenderer.startWidth = width;
+        lineRenderer.endWidth = width;
+        lineRenderer.loop = true;
+        lineRenderer.useWorldSpace = true;
+        lineRenderer.positionCount = segments;
+
+        vertices = new List<Vector3>();
+
+        float angle;
+
+        for (int i = 0; i < segments; ++i)
+        {
+            angle = Mathf.Deg2Rad * i / segments * 360;
+
+            vertices.Add(new Vector3(Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius, 0.0f));
+        }
+
+        for (int i = 0; i < segments; ++i)
+        {
+            lineRenderer.SetPosition(i, vertices[i]);
+        }
+    }
+
+    public float Radius { get { return radius; } }
+}
---
+++ Assets/Scripts/ThrowerMovement.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class ThrowerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private BoundaryRing boundaryRing;
+
+    private Vector3 ringPosition;
+    private float ringRadius;
+    private float throwerRadius;
+
+    private void Start()
+    {
+        ringPosition = boundaryRing.transform.position;
+        ringRadius = boundaryRing.Radius;
+        throwerRadius = transform.GetChild(0).transform.localScale.x * 0.5f;
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, vertical, 0.0f);
+
+        if (direction.magnitude > 1.0f)
+        {
+            direction.Normalize();
+        }
+
+        Vector3 nextPosition = transform.position + direction* moveSpeed *Time.deltaTime;
+
+        if (ringRadius - throwerRadius < (nextPosition - ringPosition).magnitude)
+        {
+            nextPosition = (nextPosition - ringPosition).normalized * (ringRadius - throwerRadius);
+        }
+
+        transform.position = nextPosition;
+    }
+}
